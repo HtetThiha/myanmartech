de914d2c9a0bf61ead486c77a2c5494dd13788bd
Htet Thiha <htetlthiha@gmail.com>
2021-03-16 12:33:35 -0700

    added raw data pull up till 0315

--- pulldata.py
+++ pulldata.py
@@ -4,7 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 import time
+from datetime import datetime
+import uuid
+import pandas as pd
+from sqlalchemy import create_engine
 
+
+engine = create_engine('mysql+mysqlconnector://<user>:<password>@localhost/ingestdata?auth_plugin=mysql_native_password', echo=False)
+connection = engine.raw_connection()
+
+items = []
+stableitems = []
 
 # Step 1) Open Firefox 
 driver = webdriver.Firefox()
@@ -14,8 +24,8 @@ driver.get("http://www.facebook.com")
 username = driver.find_element_by_id("email")
 password = driver.find_element_by_id("pass")
 submit   = driver.find_element_by_name("login")
-username.send_keys("6503849237")
-password.send_keys("B0ga13:87")
+username.send_keys("username")
+password.send_keys("password")
 # Step 4) Click Login
 submit.click()
 
@@ -24,7 +34,30 @@ wait = WebDriverWait(driver,10)
 position = 1
 keepgoing = 1
 
-driver.get("https://www.facebook.com/khitthitnews/")
+# driver.get("https://www.facebook.com/khitthitnews/")
+# driver.get("https://www.facebook.com/MizzimaDaily/")
+# driver.get("https://www.facebook.com/rfaburmese")
+# driver.get("https://www.facebook.com/myanmarnownews")
+# driver.get("https://www.facebook.com/VoA.Burmese.News/")
+# driver.get("https://www.facebook.com/theirrawaddyburmese/")
+# driver.get("https://www.facebook.com/DVBTVNews/")
+# driver.get("https://www.facebook.com/ElevenMediaGroup/")
+
+# driver.get("https://www.facebook.com/NewsWatchJournal/")
+# driver.get("https://www.facebook.com/kamayutmedia/")
+# driver.get("https://www.facebook.com/frontiermyanmar.net/")
+# driver.get("https://www.facebook.com/crph.official.mm/")
+# driver.get("https://www.facebook.com/kumudranews/")
+
+
+
+
+
+
+
+
+
+
 
 #div[aria-posinset='13'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message']  div > div > span > div > div
 
@@ -39,6 +72,10 @@ while True:
         
     nativemessage = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message']  div > div > span")
     while nativemessage:
+        item = {}
+        item['created_at'] = datetime.now()
+        item['id'] = str(uuid.uuid4())
+        
 #         print("message at position:"+str(position)+" : "+message.text)
         position += 1
         time.sleep(SCROLL_PAUSE_TIME)
@@ -47,9 +84,20 @@ while True:
         
         #check if there is see more
         
-        timestamp = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div >div > div:nth-child(2)>div>div:nth-child(2)")
-        postlink = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div >div > div>div>a")
+        try:
+            timestamp = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div >div > div:nth-child(2)>div>div:nth-child(2)")
+#         print("timestamp: "+str(position)+" : "+timestamp.text )
+            item['timestamp'] = timestamp.text
+        except:
+            item['timestamp'] = "no time stamp"
         
+        try:
+            postlink = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div >div > div>div>a")
+#         print("postlink: "+postlink.get_attribute('href'))
+            item['postlink'] = postlink.get_attribute('href')
+        except:
+            item['postlink'] = "no postlink"
+    
         try:
             seemore = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message']  div > div > span > div:last-child > div > div")
             if seemore:
@@ -59,13 +107,22 @@ while True:
         
         except:
             print("see more native language is not there")
-        finally:
-            nativemessage = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message']  div > div > span")
         
-        seetranslation = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message'] +div > div > div > span > div")
-        if seetranslation:
-            print("see translation clicked")
-            seetranslation.click()
+        try:
+            nativemessage = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message']  div > div > span")
+#         print("nativemessage: "+nativemessage.text )
+            item['nativemessage'] = nativemessage.text
+        except:
+            item['nativemessage'] = "no message"
+        
+        
+        try:
+            seetranslation = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message'] +div > div > div > span > div")
+            if seetranslation:
+                print("see translation clicked")
+                seetranslation.click()
+        except:
+            print("see translation not there")
         
         try:
             seemoretrans = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message'] +blockquote > span > div > div >span > div:last-child >div > div")
@@ -73,42 +130,65 @@ while True:
             time.sleep(SCROLL_PAUSE_TIME)
         except:
             print("see more traslated language is not there")
-        finally:
-            time.sleep(SCROLL_PAUSE_TIME)
-            translatemessage = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message'] +blockquote")
             
+        try:
+            translatemessage = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div[data-ad-preview='message'] +blockquote")
+            #         print("translatemessage: "+translatemessage.text )
+            item['translatedmessage'] = translatemessage.text
+        except:
+            item['translatedmessage'] = "no translated message"
         
+#         item['images'] = []
         #getting images links
+        imagelinks = ""
         try:
             images = driver.find_elements_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div > div >a >div>div>div>img")
             for image in images:
-                    print("image link: "+ image.get_attribute('src'))
+#                 print("image link: "+ image.get_attribute('src'))
+#                 item['images'].append(image.get_attribute('src'))
+                imagelinks = imagelinks + ";" + image.get_attribute('src')
         except:
             print('nested image links not there')
             
         
         try:
             singleimage = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div:nth-child(3) > div > div >a>div>div>div>div>img")
-            print("single image link: "+ singleimage.get_attribute('src'))
+#             print("single image link: "+ singleimage.get_attribute('src'))
+#             item['images'].append(singleimage.get_attribute('src'))
+            imagelinks = imagelinks + ";" + singleimage.get_attribute('src')
         except:
             print('single nested image link not there')
         
+        item['images'] = imagelinks
+        
+        
         try:
             hasmoreimages = driver.find_element_by_css_selector("div[aria-posinset='"+str(position)+"'] > div >div > div > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div > div:last-child >a>div+div>div")
-            print("more images: "+ hasmoreimages.text)
+#             print("more images: "+ hasmoreimages.text)
+            item['hasmoreimages'] = hasmoreimages.text
         except:
             print("no additional images")
-            
-            
-        print("timestamp: "+str(position)+" : "+timestamp.text )
-        print("postlink: "+postlink.get_attribute('href'))
-        print("nativemessage: "+nativemessage.text )
-        print("translatemessage: "+translatemessage.text )
+            item['hasmoreimages'] = '0'
+        
+        
+        
+        items.append(item)
+        stableitems.append(item)
+        
+        if position%10 == 0:
+            series = pd.DataFrame(None)
+            series = pd.DataFrame.from_dict(items)
+            series.to_sql('kumudranews2', con=engine, if_exists='append', index= False)
+            items = []
+
         
         nativemessage.location_once_scrolled_into_view
+        driver.execute_script("window.scrollTo(0, window.scrollY + 400)")
         time.sleep(4)
-        
-    driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
+    
+#     keepgoing = 0
+#     break
+#     driver.execute_script("window.scrollTo(0, window.scrollY + 200)")
         
     # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -126,6 +206,10 @@ while True:
     
 
 driver.quit()
+
+
+stableitems
+
 # first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "div[aria-posinset='"+str(position)+"']")))
 # if first_result:
 #     message = driver.find_element_by_css_selector("div[data-ad-preview='message']  div > div > span > div > div")
